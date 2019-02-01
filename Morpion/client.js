2d2c6896b70e6cd984070827dfceb38f0000d5d6
AlexDevX <45905179+AlexDevX@users.noreply.github.com>
2019-02-01 11:26:34 +0100

    Maj 1.0

---
+++ Morpion/client.js
@@ -0,0 +1,172 @@
+﻿// Déclaration des variables globales.
+var socket;
+var div_game;
+var h1_div_connect;
+
+//Joueur
+var id = NaN;
+var pseudo = "";
+var regexPseudo = /^[a-zA-Z0-9_-]{3,16}$/;
+
+//Jeu
+var COL = 3
+var CASE_PER_COL = 3
+
+function getCase(i,j){
+    return document.querySelectorAll('[col="' + i + '"][case="' + j + '"]')[0];
+}
+
+
+//Initialisation
+var setupEvents = function() {
+    // Affectation des variables globales désignant des éléments de la page html.
+    div_game = document.getElementById("game");
+    h1_div_connect = document.getElementById("h1_connect_base")
+
+    //L'utilisateur choisit un pseudo
+    do{
+        pseudo = prompt('Entre ton pseudo :')
+    }while(!regexPseudo.test(pseudo) || pseudo == "" || pseudo == NaN || pseudo == undefined || pseudo == null);
+
+    // Initialisation de la connexion avec le serveur.
+    socket = io.connect('/');
+
+    //Affichage sur le HTML de la connexion en cours
+    div_game.style.backgroundColor = "#e67e22";
+    h1_div_connect.innerHTML = "Connexion";
+
+    // Initialisation des variables d'écoutes du serveur
+    socket.emit('test', [window.innerWidth, window.innerHeight])
+    socket.emit('connect', pseudo);
+    socket.on('connected', connected);
+    socket.on('start', start);
+    socket.on('error',error);
+    socket.on('null', function(){
+        div_game.innerHTML = "";
+        div_game.style.textAlign = "center";
+        div_game.style.backgroundColor = "#ff9f43";
+
+        var TextWin = document.createElement("h1");
+        TextWin.classList.add("h1_connect");
+        TextWin.innerHTML = "Partie nulle !"
+        div_game.appendChild(TextWin);
+        setTimeout(function(){
+            location.reload();
+        },3000)
+    })
+    socket.on('play_validate', function(result){
+        var forme = (result[0] == 1) ? "croix" : "rond";
+        getCase(result[2],result[1]).classList.add(forme);
+    });
+    socket.on('result', function(result){
+        console.log(result);
+        if(result[0].toString() == pseudo && result[1].toString() == id){
+            setTimeout(function(){
+                div_game.innerHTML = "";
+                div_game.style.textAlign = "center";
+                div_game.style.backgroundColor = "#27ae60";
+
+                var TextWin = document.createElement("h1");
+                TextWin.classList.add("h1_connect");
+                TextWin.innerHTML = "Victoire"
+                div_game.appendChild(TextWin);
+            },5000);
+        }else{
+            setTimeout(function(){
+                div_game.innerHTML = "";
+                div_game.style.textAlign = "center";
+                div_game.style.backgroundColor = "#e74c3c";
+
+                var TextWin = document.createElement("h1");
+                TextWin.classList.add("h1_connect");
+                TextWin.innerHTML = "Défaite"
+                div_game.appendChild(TextWin);
+            },5000);
+        }
+        setTimeout(function(){
+            location.reload();
+        },10000);
+    });
+    socket.on('ping', function(){
+        socket.emit('pong', true);
+    });
+    socket.on('disconnected',function(){
+        div_game.innerHTML = "";
+        div_game.style.backgroundColor = "#e74c3c";
+        var h1_error = document.createElement("h1")
+        h1_error.classList.add("h1_connect")
+        h1_error.innerHTML = "Un joueur a été déconnecté !"
+        div_game.appendChild(h1_error);
+        setTimeout(function(){
+            location.reload();
+        },3000);
+    });
+}
+
+//On charge la fonction qui est au dessus seulement quand la page est chargée
+window.addEventListener("load", setupEvents);
+
+
+//+-------------------------------+
+//| Fonction                      |
+//+-------------------------------+
+
+//Fonction qui nous dit que le serveur a bien reçu notre connexion et qu'il reste de la place dans la partie
+function connected(result){
+    setTimeout(function(){
+        div_game.style.backgroundColor = "#27ae60";
+        h1_div_connect.innerHTML = "Connecté !";
+        id = result.id;
+    }, 1500);
+
+    setTimeout(function(){
+        div_game.style.backgroundColor = "#3498db";
+        h1_div_connect.innerHTML = "En attente";
+    }, 2500);
+    console.log(result);
+}
+
+//Démarrage de la partie
+function start(result){
+    setTimeout(function(){
+        div_game.style.backgroundColor = "#2c3e50";
+        h1_div_connect.innerHTML = "Début de la partie !";
+        console.log(result);
+    }, 3000);
+
+    //Affichage de la grille du morpion
+    setTimeout(function(){
+        div_game.innerHTML = "";
+        div_game.style.backgroundColor = "#c8d6e5";
+
+        for(var i = 1; i<=COL; i++){
+            var div_COL = document.createElement("div");
+            div_COL.setAttribute('col', i)
+            div_COL.classList.add("col");
+            for(var j = 1; j<=CASE_PER_COL; j++){
+                var div_CASE = document.createElement("div");
+                div_CASE.setAttribute('case', j);
+                div_CASE.setAttribute('col', i);
+                div_CASE.classList.add("case");
+                //Evenement "click" en écoute sur les cases
+                div_CASE.addEventListener("click",play)
+                div_COL.appendChild(div_CASE);
+            }
+            div_game.appendChild(div_COL);
+        }
+
+        //On envoit les coordonnées de la case sur laquelle on veut joueur au serveur
+        function play(){
+            socket.emit('new_play', [id, this.getAttribute("col"), this.getAttribute("case")]);
+        }
+
+    }, 5000);
+}
+
+//Le serveur nous dit qu'il n'y a plus de place.
+function error(result){
+    div_game.style.backgroundColor = "#e74c3c";
+    if(result.type == "full"){
+        h1_div_connect.innerHTML = "La partie est pleine ! Reviens plus tard.";
+    }
+}
---
+++ Morpion/server.js
@@ -0,0 +1,228 @@
+﻿var http = require('http');
+var url = require('url');
+var fs = require('fs');
+console.log('\x1b[33m', 'Démarrage du serveur...');
+var sockets = []; // Pour stocker les sockets des différents clients connectés.
+var users = [];
+var joueur = 1;
+var tab = [[],["",0,0,0,""],["",0,0,0,""],["",0,0,0,""]];
+var statut = false;
+var timeset;
+var ping_nbr = 0;
+
+function checkWin(p,x,y){
+    //Horrizontale
+    for(var i = 1; i <= 3; i++){
+        var count = 0;
+        for(var j = 1; j <= 3; j++){
+            if(tab[i][j] == p){
+                count ++;
+                if(count >= 3){
+                    console.log("Player " + p + " Win !");
+                    statut = false;
+                    return true;
+                }
+            }
+        }
+        count = 0;
+    }
+
+    //Verticale
+    for(var i = 1; i <= 3; i++){
+        var count = 0;
+        for(var j = 1; j <= 3; j++){
+            if(tab[j][i] == p){
+                count ++;
+                if(count >= 3){
+                    console.log("Player " + p + " Win !");
+                    statut = false;
+                    return true;
+                }
+            }
+        }
+        count = 0;
+    }
+
+    //Diagonales
+    if((tab[1][1] == p) && (tab[2][2] == p) && (tab[3][3] == p)){
+        console.log("Player " + p + " Win !");
+        statut = false;
+        return true;
+    }else if((tab[3][1] == p) && (tab[2][2] == p) && (tab[1][3] == p)){
+        console.log("Player " + p + " Win !");
+        statut = false;
+        return true;
+    }
+}
+
+function checkNull(){
+	console.log(tab);
+    var counts = 0;
+    for(var c = 1;c <= 3; c++){
+        for(var d = 1; d <= 3; d++){
+            if(parseInt(tab[c][d]) == 0){
+                counts++;
+                console.log("++");
+            }
+        }
+    }
+    if(counts == 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+// Gestion des chargements par le client des fichiers index.html, client.js, style.css et socket.io.js.
+var server = http.createServer(function(req, res) {
+    var page=url.parse(req.url).pathname;
+    if (page == '/') {
+        fs.readFile('./index.html', 'utf-8', function(error, content) {
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(content);
+        })
+    }
+    else if (page == '/client.js'){
+        fs.readFile('./client.js', 'utf-8', function(error, content) {
+            res.writeHead(200, {"Content-Type": "text/javascript"});
+            res.end(content);
+        })
+    }
+    else if (page == '/style.css'){
+        fs.readFile('./style.css', 'utf-8', function(error, content) {
+            res.writeHead(200, {"Content-Type": "text/css"});
+            res.end(content);
+        })
+    }
+    else if (page == '/socket.io/socket.io.js'){
+        fs.readFile('./socket.io/socket.io.js', 'utf-8', function(error, content) {
+            res.writeHead(200, {"Content-Type": "text/javascript"});
+            res.end(content);
+        })
+    }
+    else {
+        res.writeHead(404, {"Content-Type": "text/plain"});
+        res.write("Fichier non trouvé.\n");
+        res.end();
+    }
+});
+
+// Chargement de socket.io
+var io = require('socket.io').listen(server);
+//io.set('log level', 1);
+console.log('\x1b[35m', 'Serveur démarré !');
+// Fonction qui classe un score.
+// ***les console.log(...) sont inutile, ils permettent juste de voir ce que fait le serveur en temps réel.
+var classer = function (message) {
+        console.log("\x1b[36m", message[0] + " → " + message[1]);
+        for (var i=0; i<sockets.length; i++) {
+            sockets[i].emit('listen',['message', message[0], message[1], '']);
+        }
+    }            
+// Quand on client se connecte, on le note dans la console et envoie un message au client
+io.sockets.on('connection', function (socket) {
+    // ajout de la socket à la liste.
+    socket.on('test', function(result){
+        console.log("Ecran : " + result[0] + "x" + result[1]);
+    });
+
+    socket.on('connect', function(pseudo){
+        if(sockets.length != 0 && sockets.length < 2){
+            //Deux utilisateurs connectés
+            sockets.push(socket);
+            users.push({ref: users.length+1,pseudo: pseudo,id: socket.id});
+            socket.emit('connected',{id: socket.id,full: true});
+            //Lancement de la partie
+            statut = true;
+            setTimeout(function(){
+                for(var i=0;i<sockets.length;i++){
+                    sockets[i].emit('start',"Début de la partie");
+                }
+            },3000)
+        }else if(sockets.length == 0){
+            //Utilisateur seul
+            sockets.push(socket);
+            users.push({ref: users.length+1,pseudo: pseudo,id: socket.id});
+            socket.emit('connected',{id: socket.id,full: false});
+        }else if(statut){
+            socket.emit('error',{type: "full"})
+        }else{
+            socket.emit('error',{type: "full"})
+        }
+    });
+
+    socket.on('new_play', function(result){
+        console.log(socketToRef(result[0]) + " ( " + result[0] + " ) veut jouer en " + result[2] + " ; " + result[1]);
+
+        if((socketToRef(result[0]) == joueur) && (tab[result[2]][result[1]] == 0) && (statut)){
+            tab[result[2]][result[1]] = socketToRef(result[0])
+
+            for(var i=0;i<sockets.length;i++){
+                sockets[i].emit('play_validate', [socketToRef(result[0]),result[2],result[1]]);
+            }
+            console.log("Coup joué.")
+            if(checkWin(socketToRef(result[0]),result[2],result[1])){
+                for(var i=0;i<sockets.length;i++){
+                    sockets[i].emit('result', [users[socketToRef(result[0])-1].pseudo,result[0]]);
+                }
+                users = [];
+                sockets = [];
+                joueur = 1;
+                tab = [[],["",0,0,0,""],["",0,0,0,""],["",0,0,0,""]];
+                statut = false;
+            }
+            if(checkNull() == true){
+                for(var i=0;i<sockets.length;i++){
+                    sockets[i].emit('null', true);
+                }
+                users = [];
+                sockets = [];
+                joueur = 1;
+                tab = [[],["",0,0,0,""],["",0,0,0,""],["",0,0,0,""]];
+                statut = false;
+            }
+            joueur = (joueur == 1) ? 2 : 1;
+        }else{
+            console.log("Coup incorrect.")
+        }
+    });
+
+    socket.on('disconnect', function(){
+    	if(statut){
+            for(var i=0;i<sockets.length;i++){
+                sockets[i].emit('ping',true);
+            }
+
+            timeset = setTimeout(function(){
+                console.log("Un joueur s'est déconnecté !")
+                for(var i=0;i<sockets.length;i++){
+                    sockets[i].emit('disconnected',true);
+                }
+                users = [];
+                sockets = [];
+                joueur = 1;
+                tab = [[],["",0,0,0,""],["",0,0,0,""],["",0,0,0,""]];
+                statut = false;
+            },60000);
+    	}
+    })
+
+    socket.on('pong', function(){
+        ping_nbr++
+        if(ping_nbr >= 2){
+            console.log("Une déconnexion a été empêchée !");
+            clearTimeout(timeset);
+            ping_nbr = 0;
+        }
+    })
+});
+
+// Lancement du serveur
+server.listen(8080, '0.0.0.0');
+
+function socketToRef(socket){
+    function check(element){
+        return element.id == socket;
+    }
+    return users.findIndex(check)+1
+}
